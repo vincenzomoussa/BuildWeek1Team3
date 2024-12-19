a4a2b846fd8b62325c36e0ba17e32ad17c1e63d3
Giorgia <giorgiamannino@hotmail.com>
2024-12-19 10:25:36 +0100

    modifiche

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,10 +115,10 @@ const questionCountEl = document.querySelector('#question');
 const options = [];
 
 //Funzione per far accettare obbligatoriamente Termini e Condizioni
-function page2() {
+function page1() {
 
     const checkbox = document.querySelector("#check")
-    const button = document.querySelector("#page2")
+    const button = document.querySelector(".bottoni")
 
     button.addEventListener('click', (event) => {
         if (!checkbox.checked) {
@@ -130,11 +130,11 @@ function page2() {
     });
 }
 if(document.location.pathname === "/index.html") {
-    page2()
+    page1()
 }
 //Funzione per il bottone di index3 
-function page4() {
-    const button2 = document.querySelector("#page4")
+function page3() {
+    const button2 = document.querySelector("#bottone3")
     button2.addEventListener('click', (event) => {
         event.preventDefault()
         window.location.href = "./index4.html";
@@ -142,12 +142,12 @@ function page4() {
 )}
 
 if(document.location.pathname === "/index3.html") {
-        page4()
+        page3()
     }
 //Funzione per il bottone di index4
 
-function pageEpicode() {
-    const button3 = document.querySelector("#pageEpicode")
+function page4() {
+    const button3 = document.querySelector(".bottoni")
     button3.addEventListener('click', (event) => {
         event.preventDefault()
         window.location.href = "https://epicode.com/it/?utm_source=adwords&utm_campaign=Brand&utm_adgroup=brandphrase&utm_term=epicode&utm_medium=ppc&hsa_acc=1246633295&hsa_cam=11897141170&hsa_grp=115607542316&hsa_ad=639941754622&hsa_src=g&hsa_tgt=kwd-1083842420783&hsa_kw=epicode&hsa_mt=p&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=CjwKCAiA34S7BhAtEiwACZzv4UfJd98I-u7uRZliuXsVMj--n4Cyblftux0gS-riLEmufJutDWyTehoCc6IQAvD_BwE";
@@ -155,7 +155,7 @@ function pageEpicode() {
 )}
 
 if(document.location.pathname === "/index4.html") {
-        pageEpicode()
+        page4()
     }
 
 
